d65a3d6af44863c6d51ad3d5ed994f5c4a5f790f
Paul Walker <paul@nucivic.com>
2016-02-19 13:53:35 +0000

    remove a bunch of react boilerplate

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-import Carto from './recline.backend.js';
+'use strict';
+// relies on Es2Sql global 
+
+const backend = {
+  __type__: 'cartodb',
+  fetch: (dataset) => {
+    const query = dataset.query || {};
+    query.table = dataset.table;
+    const data ={ q: Es2Sql.translate(query) };
+    const url = '//' + dataset.user + '.cartodb.com/api/v2/sql';
+    $.get(url, data)
+      .success((res) => {
+        console.log('success', res);
+      })
+      .error((a,b,c) => {
+        console.log('err',a,b,c);
+      });
+  }
+};
+
+export {my as Backend};
--- webpack.config.js
+++ webpack.config.js
@@ -45,8 +45,6 @@ var webpack = require('webpack');
 var path = require('path');
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:5000',
-    'webpack/hot/dev-server',
     './src/index'
   ],
   output: {
@@ -59,14 +57,13 @@ module.exports = {
   },
   devtool: 'eval-source-map',
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NoErrorsPlugin()
   ],
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        loaders: ['react-hot', 'babel'],
+        loaders: ['babel'],
         include: path.join(__dirname, 'src')
       }
     ]
