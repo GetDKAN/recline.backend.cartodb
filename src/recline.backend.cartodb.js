de7a28b66a1cefc7a9ac40255042f2dbf21f6795
Paul Walker <paul@nucivic.com>
2016-02-23 21:59:49 +0000

    Fix fields format

--- src/recline.backend.cartodb.js
+++ src/recline.backend.cartodb.js
@@ -8,35 +8,60 @@ recline.Backend.cartodb = recline.Backend.cartodb || {};
 
   my._type = 'cartodb';
   my.fetch = function (dataset) {
+    if (dataset.url) {
+      dataset.user = privates._parseDatasetUrl(dataset.url);
+      dataset.table = privates._parseTableName(dataset.url);
+    }
+    console.log('cdbf0', dataset);
     var dfd = new Deferred();
-    var query = Es2sql.Es2Sql.translate(dataset.query || privates._defaultQuery(dataset));
     var sql = cartodb.SQL({ user: dataset.user });
-    console.log('cdbf1', dataset, query, privates._defaultQuery(dataset));
-    sql.execute("SELECT * FROM " + dataset.table + " LIMIT 10").done(function (data) {
-      console.log(data);
+    var query = dataset.query || privates._defaultQuery(dataset);
+    query.table = dataset.table;
+    console.log('cdbf0.5', query);
+    sql.execute(Es2sql.translate(query)).done(function (data) {
+      console.log('cdf0.6', data);
+      var fields = _.keys(data.fields).map(function(val, i) {
+        return {id: val};
+      });
+      console.log('cdbf1', fields, dataset);
       dfd.resolve({
-        records: data.rows,
-        fields: data.fields,
-        useMemoryStore: true
+        fields: fields,
+        rows:  data.rows,
+        useMemoryStore: false
       });
     });
+    dataset.isFetch = true;
     return dfd.promise();
   };
 
-  my.query = function (dataset) {
+  my.query = function (queryObj, dataset) {
+    if (dataset.isFetch)  {
+      console.log('isFetchg');
+      dataset.isFetch = false;
+      return;
+    }
+    if (dataset.url) {
+      dataset.user = privates._parseDatasetUrl(dataset.url);
+      dataset.table = privates._parseTableName(dataset.url);
+    }
+    console.log('cdbQ0', queryObj, dataset);
     var dfd = new Deferred();
     var sql = cartodb.SQL({ user: dataset.user });
-    var query = dataset.query;
+    var query = queryObj || privates._defaultQuery(dataset);
     query.table = dataset.table;
-  
-    sql.execute(Es2sql.Es2Sql.translate(query)).done(function (data) {
-      dfd.resolve({
-        records: data.rows,
-        fields: data.fields,
-        useMemoryStore: true
+    console.log('cdbQ1', query);
+    sql.execute(Es2sql.translate(query)).done(function (data) {
+      var fields = _.keys(data.fields).map(function(val, i) {
+        return {id: val};
       });
-      return dfd.promise();
+      console.log('cdbQ3', data, fields);
+      dfd.resolve({
+        total: data.total_rows,
+        hits: data.rows,
+        fields: fields
+      });
     });
+    return dfd.promise();
   };
 
   privates._defaultQuery = function (dataset) {
@@ -44,8 +69,25 @@ recline.Backend.cartodb = recline.Backend.cartodb || {};
       table: dataset.table,
       user: dataset.user,
       from: 0,
-      size: 10
+      size: 100
     }
   };
+
+  privates._parseDatasetUrl = function (url) {
+    var s = url.replace(/http(s*):\/\//g, '');
+    s = s.split('.')[0];
+    console.log('>>',s);
+    return s;
+  }
+
+  privates._parseTableName = function (url) {
+    var s = url.match(/q=(.*)/g);
+    console.log(s);
+    s[0].replace('q=','');
+    var reg = /(FROM )([^\s]+)/g;
+    var t = reg.exec(decodeURIComponent(url));
+    console.log('t',t[2]);
+    return t[2];
+  }
 
 })(recline.Backend.cartodb);
